bb97c1dd5b306a608c517e9c67fcde9855568fd5
ishitva07 <rathore.ishitva01@gmail.com>
2026-03-21 15:17:27 +0530

    Refactor scroll reveal animation and typing effect

--- script.js
+++ script.js
@@ -55,14 +55,4 @@ let scrolled = (winScroll / height) * 100;
 document.querySelector(".scroll-bar").style.width = scrolled + "%";
 
 };
-window.addEventListener("scroll", function(){
 
-let scroll = window.scrollY;
-
-document.querySelector(".layer2").style.transform =
-"translateY(" + scroll * 0.1 + "px)";
-
-document.querySelector(".layer4").style.transform =
-"translateY(" + scroll * 0.2 + "px)";
-
-});
